8a0b4aef2c5b89d8baa0763af32eea4680cbb2f4
rebi00dev <skfkdRp489@gmail.com>
2026-01-28 18:15:32 +0900

    setup config folder

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import find_packages, setup
 from glob import glob
+import os
+
 package_name = 'smartwarehouse'
 
 setup(
@@ -11,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob('launch/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
