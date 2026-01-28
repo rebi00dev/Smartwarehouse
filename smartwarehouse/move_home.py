f437615e1b6e5c242a62d99f40072f818c208735
rebi00dev <skfkdRp489@gmail.com>
2026-01-28 18:17:21 +0900

    move home action

---
+++ smartwarehouse/move_home.py
@@ -0,0 +1,22 @@
+
+import DR_init
+from .base_action import BaseAction
+
+VELOCITY, ACCURACY = 80, 60
+
+class MoveHomeAction(BaseAction):
+    def __init__(self, node, DR, poses):
+        DR_init.__dsr__node = node
+
+        self.DR = DR
+        self.hom_pose_pose = poses['home']
+        self.node = node
+
+        self.success = False
+
+    # 실행되는 동작
+    def execute(self):
+        # home 위치로 로봇팔 이동
+        self.DR.movej(self.home_pose, vel = VELOCITY, acc=ACCURACY)
+
+
