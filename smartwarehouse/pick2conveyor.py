dd01f841afc3e093afb0f3ee4673e309322988fc
rebi00dev <skfkdRp489@gmail.com>
2026-01-28 18:29:41 +0900

    modify llm to yolo

--- smartwarehouse/pick2conveyor.py
+++ smartwarehouse/pick2conveyor.py
@@ -26,7 +26,6 @@ DR = None
 class PickAction(BaseAction):
     def __init__(self, node, DR, poses, object):
         DR_init.__dsr__node = node
-
         self.DR = DR
         self.pick_pose = poses
         self.object = object
@@ -36,153 +35,82 @@ class PickAction(BaseAction):
         self.success = False
 
 
-        self.get_position_client = node.create_client(SrvDepthPosition, "/get_3d_position")
+        self.get_position_client = node.create_client(SrvDepthPosition, "/depth")
         while not self.get_position_client.wait_for_service(timeout_sec=3.0):
             self.get_logger().info("Waiting for get_depth_position service...")
         self.get_position_request = SrvDepthPosition.Request()
 
+        self.gripper = GripperController()
+
     # 실행되는 동작
     def execute(self):
-        gripper = GripperController()
-        if self.ingredient in SHAKER_SET:
-            # 그리퍼 열기
-            self.gripper.move_gripper(1100, force_val=200)
-            time.sleep(1)
+        
+        self.gripper.open()
+        time.sleep(1)
 
-            DR.movel(self.pick_pose["shaker"]["task"], vel = VELOCITY, acc=ACCURACY)
+        # 거리 계산 (음료와 가니쉬일 경우)
+            
+            self.get_position_request.target = self.object
+            self.get_logger().info(f"Requesting 3D position for {self.object}")
+            depth_future = self.get_position_client.call_async(self.get_position_request)
+            rclpy.spin_until_future_complete(self.node, depth_future)
 
-            # 그리퍼 닫기
-            self.gripper.move_gripper(500, force_val=300)
-            time.sleep(1)
+            if depth_future.result():
+                pos = depth_future.result().depth_position.tolist()
+                self.get_logger().info(f"Object {self.object} detected at {pos}")
 
-            # 충돌 회피
-            pick_after = self.pick_pose["shaker"]["task"].copy()
-            pick_after[2] = self.pick_pose["shaker"]["task"][2] + 50
-            DR.movel(pick_after, vel = VELOCITY, acc=ACCURACY)
+                gripper2cam_path = os.path.join(package_path, "resource", "T_gripper2camera.npy")
+                robot_posx = self.DR.get_current_posx()[0]
+                td_coord = self.transform_to_base(pos, gripper2cam_path, robot_posx)
 
-        else:
-             
-            # 예외처리 반복
-            for attempt in range(self.max_retry):
-            # 초기 Detect 위치로 로봇팔 이동
-                if self.ingredient in BEVERAGE_SET:
-                    DR.movej(self.pick_pose["beverage_detect"]["joint"], vel=VELOCITY, acc=ACCURACY)
-                elif self.ingredient in GARNISH_SET:
-                    DR.movej(self.pick_pose["garnish_detect"]["joint"], vel=VELOCITY, acc=ACCURACY)
+                if self.object in BEVERAGE_SET: 
+                    if td_coord[1] and sum(td_coord) != 0:
+                                            td_coord[1] -= 40  # DEPTH_OFFSET
+                                        # td_coord[1] = max(td_coord[1], 2)  # MIN_DEPTH: float = 2.0
+                elif self.object in GARNISH_SET:
+                    if self.object == 'lime':
+                        if td_coord[2] and sum(td_coord) != 0:
+                                            td_coord[2] -= 12  # DEPTH_OFFSET
+                    elif self.object == 'cherry':
+                        if td_coord[2] and sum(td_coord) != 0:
+                                            td_coord[2] -= 5
+
+
+                # 계산된 결과
+                target_pos = list(td_coord[:3]) + robot_posx[3:]
+                grap_pos = target_pos.copy()
+                apporach_pos = target_pos.copy()
+                apporach_pos[2] = target_pos[2] + 30
+
+                # Detect 위치로 이동
+                self.DR.movel(target_pos, vel=VELOCITY, acc=ACCURACY)
+                # self.DR.movel(grap_pos, vel=VELOCITY, acc=ACCURACY)
+                # self.DR.movel(grap_pos, vel=VELOCITY, acc=ACCURACY)
+                self.DR.mwait()
+
+                # 그리퍼 닫기(close_gripper)
+                self.gripper.close()
                 
-                
-                # 그리퍼 열기(open_gripper)
-                self.gripper.move_gripper(1100, force_val=200)
-                DR.mwait()
-                
-
-                # 거리 계산 (음료와 가니쉬일 경우)
-                if self.ingredient in (BEVERAGE_SET | GARNISH_SET):
-                    
-                    self.get_position_request.target = self.ingredient
-                    self.get_logger().info(f"Requesting 3D position for {self.ingredient}")
-                    depth_future = self.get_position_client.call_async(self.get_position_request)
-                    rclpy.spin_until_future_complete(self.node, depth_future)
-
-                    if depth_future.result():
-                        pos = depth_future.result().depth_position.tolist()
-                        self.get_logger().info(f"Object {self.ingredient} detected at {pos}")
-
-                        gripper2cam_path = os.path.join(package_path, "resource", "T_gripper2camera.npy")
-                        robot_posx = DR.get_current_posx()[0]
-                        td_coord = self.transform_to_base(pos, gripper2cam_path, robot_posx)
-
-                        if self.ingredient in BEVERAGE_SET: 
-                            if td_coord[1] and sum(td_coord) != 0:
-                                                    td_coord[1] -= 40  # DEPTH_OFFSET
-                                                # td_coord[1] = max(td_coord[1], 2)  # MIN_DEPTH: float = 2.0
-                        elif self.ingredient in GARNISH_SET:
-                            if self.ingredient == 'lime':
-                                if td_coord[2] and sum(td_coord) != 0:
-                                                    td_coord[2] -= 12  # DEPTH_OFFSET
-                            elif self.ingredient == 'cherry':
-                                if td_coord[2] and sum(td_coord) != 0:
-                                                    td_coord[2] -= 5
-
-
-                        # 계산된 결과
-                        target_pos = list(td_coord[:3]) + robot_posx[3:]
-                        grap_pos = target_pos.copy()
-                        apporach_pos = target_pos.copy()
-                        apporach_pos[2] = target_pos[2] + 30
-
-                        # Detect 위치로 이동
-                        DR.movel(target_pos, vel=VELOCITY, acc=ACCURACY)
-                        # DR.movel(grap_pos, vel=VELOCITY, acc=ACCURACY)
-                        # DR.movel(grap_pos, vel=VELOCITY, acc=ACCURACY)
-                        DR.mwait()
-
-                        # 그리퍼 닫기(close_gripper)
-                        if self.ingredient in BEVERAGE_SET:
-                            if self.ingredient == 'blue-juice':
-                                grap_pos[2] -= 20
-                                DR.movel(grap_pos, vel=VELOCITY, acc=ACCURACY)
-                                self.gripper.move_gripper(400, force_val=400)
-                                time.sleep(1)
-                            else:     
-                                DR.movel(grap_pos, vel=VELOCITY, acc=ACCURACY)
-                                self.gripper.move_gripper(400, force_val=400)
-                                time.sleep(1)
-                            DR.movel(grap_pos, vel=VELOCITY, acc=ACCURACY)
-                            self.gripper.move_gripper(400, force_val=400)
-                            time.sleep(1)
-                        elif self.ingredient in GARNISH_SET:
-                            if self.ingredient == 'lime':
-                                self.gripper.move_gripper(400, force_val=350)
-                                time.sleep(1)
-                            elif self.ingredient == 'cherry':
-                                self.gripper.move_gripper(200, force_val=100)
-                                time.sleep(5)
-                        
-                        # 예외처리 체크
-                        status = self.gripper.get_status()
-                        if status[1]:  # grip detected
-                            self.success = True
-                            break
-                        else:
-                            self.gripper.move_gripper(1100, force_val=200)
-                            self.get_logger().warn(f"Grip not detected (attempt {attempt+1}), retrying...")
-
-
-                    # 뒤로 이동
-                    DR.movel(apporach_pos, vel=VELOCITY, acc=ACCURACY)
-                    if self.ingredient in BEVERAGE_SET:
-                        DR.movel(apporach_pos, vel=VELOCITY, acc=ACCURACY)
-                        DR.movel(self.pick_pose["beverage_detect"]["task"], vel=VELOCITY, acc=ACCURACY)
-                    elif self.ingredient in GARNISH_SET:
-                        DR.movel(self.pick_pose["garnish_detect"]["task"], vel=VELOCITY, acc=ACCURACY)
-
-                    if attempt == 3:
-                        raise KeyError
-
-
-              
-    def robot_control(self):
-        target_list = []
-        get_keyword_future = self.get_keyword_client.call_async(self.get_keyword_request)
-        rclpy.spin_until_future_complete(self.node, get_keyword_future)
-        if get_keyword_future.result() and get_keyword_future.result().success:
-            get_keyword_result = get_keyword_future.result()
-
-            target_list = get_keyword_result.message.split()
-
-            for target in target_list:
-                target_pos = self.get_target_pos(target)
-                if target_pos is None:
-                    self.get_logger().warn("No target position")
+                # 예외처리 체크
+                status = self.gripper.get_status()
+                if status[1]:  # grip detected
+                    self.success = True
+                    break
                 else:
-                    self.get_logger().info(f"target position: {target_pos}")
-                    self.pick_and_place_target(target_pos)
-                    self.init_robot()
+                    self.gripper.move_gripper(1100, force_val=200)
+                    self.get_logger().warn(f"Grip not detected (attempt {attempt+1}), retrying...")
 
-        else:
-            self.get_logger().warning(f"{get_keyword_result.message}")
-            return
+
+            # 뒤로 이동
+            self.DR.movel(apporach_pos, vel=VELOCITY, acc=ACCURACY)
+            if self.object in BEVERAGE_SET:
+                self.DR.movel(apporach_pos, vel=VELOCITY, acc=ACCURACY)
+                self.DR.movel(self.pick_pose["beverage_detect"]["task"], vel=VELOCITY, acc=ACCURACY)
+            elif self.object in GARNISH_SET:
+                self.DR.movel(self.pick_pose["garnish_detect"]["task"], vel=VELOCITY, acc=ACCURACY)
+
+            if attempt == 3:
+                raise KeyError
 
 
     def get_robot_pose_matrix(self, x, y, z, rx, ry, rz):
@@ -211,7 +139,7 @@ class PickAction(BaseAction):
             gripper2cam_path = os.path.join(
                 package_path, "resource", "T_gripper2camera.npy"
             )
-            robot_posx = DR.get_current_posx()[0]
+            robot_posx = self.DR.get_current_posx()[0]
             td_coord = self.transform_to_base(result, gripper2cam_path, robot_posx)
 
             if td_coord[2] and sum(td_coord) == 0:
