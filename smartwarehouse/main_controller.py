d47abfc5df96b00c0432a749b2465b43f1d92f54
rebi00dev <skfkdRp489@gmail.com>
2026-01-29 11:17:18 +0900

    add pick2conveyor with main controller

--- smartwarehouse/main_controller.py
+++ smartwarehouse/main_controller.py
@@ -13,7 +13,7 @@ from .gripper_controller import GripperController
 from .pick2conveyor import PickAction
 from .place2shelf import PlaceAction
 from .move_home import MoveHomeAction
-from .yolo import DetectAction
+from .yolo import YoloDetectAction
 from .waypoint import WaypointAction
 
 POSE_PATH = os.path.join(
@@ -36,10 +36,10 @@ def load_yaml(POSE_PATH):
 # ===================================================================================================
 # action setting
 def action_build(node, DR, poses):
-    return {'pick':lambda object: PickAction(node, dr=DR, poses=poses['Pick'], ingredient=object),
+    return {'pick':lambda target_pose: PickAction(node, dr=DR, poses=poses['Pick'], target_pose=target_pose),
             'place':lambda object, dict: PlaceAction(node, dr=DR, poses=poses['Place'], ingredient=object, dict=dict),
             'home':lambda : MoveHomeAction(node, dr=DR, poses=poses['Home']),
-            'yolo':lambda : DetectAction(node, dr=DR),
+            'yolo':lambda : YoloDetectAction(node, dr=DR),
             'waypoint':lambda : WaypointAction(node, dr=DR, poses=poses['Waypoint']),
             }
 # ===================================================================================================
@@ -58,9 +58,9 @@ def main():
     # # 툴베이스 설정 ###############################################################변경 필요
     DR.set_robot_mode(0)
 
-    # set_tool("Tool Weight v2")
-    # set_tcp("GripperDA_v2")
-    # set_ref_coord(DR.DR_BASE)
+    # DR.set_tool("Tool Weight v2")
+    # DR.set_tcp("GripperDA_v2")
+    # DR.set_ref_coord(DR.DR_BASE)
 
     DR.set_robot_mode(1)
 
@@ -81,43 +81,43 @@ def main():
         raise SyntaxError('pose.yaml,action build')
     
     
-    # ====================================================================
-    # 반복 시작
+    # 반복 시작 ====================================================================
     while True:
         try:
             # Home 위치 이동 ========================================================
             actions_build['home']().execute()
             node.get_logger().info("Home 위치 도달. 물체 인식을 시작합니다.")
 
-            # Yolo data 확인 (물건 list) =============================================
-            target_object = actions_build['yolo']().execute()
-            if not target_object:
+            # Yolo data 확인 =======================================================
+            target_name, target_pose = actions_build['yolo']().execute()
+            if not target_name:
                 node.get_logger().info("감지된 물체가 없습니다. 재시도 중...")
+                time.sleep(5)
                 continue
-            node.get_logger().info(f"타겟 감지: {target_object}")
+            node.get_logger().info(f"타겟 감지: {target_name}")
 
             # pick ================================================================= 
-            pick_success = actions_build['pick'](target_object).execute()
+            pick_success = actions_build['pick'](target_pose).execute()
             if not pick_success:
                 node.get_logger().error("물체를 잡는 데 실패했습니다. 처음으로 돌아갑니다.")
                 continue
-            node.get_logger().info(f"{target_object} pick 완료. home 위치 이동")
+            node.get_logger().info(f"{target_name} pick 완료. home 위치 이동")
 
             # home =================================================================
             actions_build['home']().execute()
-            node.get_logger().info(f"{target_object} 이동 완료. waypoint 이동 ")
+            node.get_logger().info("이동 완료. waypoint 이동 ")
 
             # waypoint =============================================================
             actions_build['waypoint']().execute()
-            node.get_logger().info(f"{target_object} 이동 완료. place 시작")
+            node.get_logger().info("이동 완료. place 시작")
 
             # place ================================================================
-            actions_build['place'](target_object, dictonary).execute()
-            node.get_logger().info(f"{target_object} place 완료. waypoint 이동.")
+            actions_build['place'](target_name).execute()
+            node.get_logger().info(f"{target_name} place 완료. waypoint 이동.")
 
             # waypoint =============================================================
             actions_build['waypoint']().execute()
-            node.get_logger().info(f"{target_object} 이동 완료. home 위치 이동")
+            node.get_logger().info(f"{target_name} 이동 완료. home 위치 이동")
 
         except Exception as e:
             node.get_logger().error(f"실행 중 예외 발생: {e}")
--- smartwarehouse/pick2conveyor.py
+++ smartwarehouse/pick2conveyor.py
@@ -24,149 +24,49 @@ DR = None
 
 
 class PickAction(BaseAction):
-    def __init__(self, node, DR, poses, object):
+    def __init__(self, node, DR, poses, target_pose):
         DR_init.__dsr__node = node
-        self.DR = DR
-        self.pick_pose = poses
-        self.object = object
         self.node = node
+        self.DR = DR
+        self.pick_pose = poses['Pick']
+        self.target_pose = target_pose
 
         self.max_retry = 3
         self.success = False
 
 
-        self.get_position_client = node.create_client(SrvDepthPosition, "/depth")
-        while not self.get_position_client.wait_for_service(timeout_sec=3.0):
-            self.get_logger().info("Waiting for get_depth_position service...")
-        self.get_position_request = SrvDepthPosition.Request()
-
         self.gripper = GripperController()
 
     # 실행되는 동작
     def execute(self):
-        
+
+        # 그리퍼를 열고 대기 포즈로 이동
         self.gripper.open()
-        time.sleep(1)
+        self.DR.movel(self.pick_pose, vel=VELOCITY, acc=ACCURACY)
+        time.sleep(0.5)
 
-        # 거리 계산 (음료와 가니쉬일 경우)
-            
-            self.get_position_request.target = self.object
-            self.get_logger().info(f"Requesting 3D position for {self.object}")
-            depth_future = self.get_position_client.call_async(self.get_position_request)
-            rclpy.spin_until_future_complete(self.node, depth_future)
+        # 물체 위로 접근
+        approach_pose = list(self.target_pose)
+        approach_pose[2] += 50.0  
+        self.DR.movel(approach_pose, vel=VELOCITY, acc=ACCURACY)
 
-            if depth_future.result():
-                pos = depth_future.result().depth_position.tolist()
-                self.get_logger().info(f"Object {self.object} detected at {pos}")
+        # 잡기 위치로 하강
+        self.DR.movel(self.target_pose, vel=VELOCITY/2, acc=ACCURACY/2)
 
-                gripper2cam_path = os.path.join(package_path, "resource", "T_gripper2camera.npy")
-                robot_posx = self.DR.get_current_posx()[0]
-                td_coord = self.transform_to_base(pos, gripper2cam_path, robot_posx)
+        #그리퍼 닫기
+        self.gripper.close()
+        time.sleep(1.0)
 
-                if self.object in BEVERAGE_SET: 
-                    if td_coord[1] and sum(td_coord) != 0:
-                                            td_coord[1] -= 40  # DEPTH_OFFSET
-                                        # td_coord[1] = max(td_coord[1], 2)  # MIN_DEPTH: float = 2.0
-                elif self.object in GARNISH_SET:
-                    if self.object == 'lime':
-                        if td_coord[2] and sum(td_coord) != 0:
-                                            td_coord[2] -= 12  # DEPTH_OFFSET
-                    elif self.object == 'cherry':
-                        if td_coord[2] and sum(td_coord) != 0:
-                                            td_coord[2] -= 5
+        # 잡기 성공 여부 판단 및 들어올리기
+        if self.gripper.grasp: 
+            # 물체를 수직으로 100mm 들어올림
+            retract_pose = list(self.target_pose)
+            retract_pose[2] += 100.0
+            self.DR.movel(retract_pose, vel=VELOCITY, acc=ACCURACY)
+            self.success = True
+        else:
+            self.DR.movel(approach_pose, vel=VELOCITY, acc=ACCURACY)
+            self.success = False
 
-
-                # 계산된 결과
-                target_pos = list(td_coord[:3]) + robot_posx[3:]
-                grap_pos = target_pos.copy()
-                apporach_pos = target_pos.copy()
-                apporach_pos[2] = target_pos[2] + 30
-
-                # Detect 위치로 이동
-                self.DR.movel(target_pos, vel=VELOCITY, acc=ACCURACY)
-                # self.DR.movel(grap_pos, vel=VELOCITY, acc=ACCURACY)
-                # self.DR.movel(grap_pos, vel=VELOCITY, acc=ACCURACY)
-                self.DR.mwait()
-
-                # 그리퍼 닫기(close_gripper)
-                self.gripper.close()
-                
-                # 예외처리 체크
-                status = self.gripper.get_status()
-                if status[1]:  # grip detected
-                    self.success = True
-                    break
-                else:
-                    self.gripper.move_gripper(1100, force_val=200)
-                    self.get_logger().warn(f"Grip not detected (attempt {attempt+1}), retrying...")
-
-
-            # 뒤로 이동
-            self.DR.movel(apporach_pos, vel=VELOCITY, acc=ACCURACY)
-            if self.object in BEVERAGE_SET:
-                self.DR.movel(apporach_pos, vel=VELOCITY, acc=ACCURACY)
-                self.DR.movel(self.pick_pose["beverage_detect"]["task"], vel=VELOCITY, acc=ACCURACY)
-            elif self.object in GARNISH_SET:
-                self.DR.movel(self.pick_pose["garnish_detect"]["task"], vel=VELOCITY, acc=ACCURACY)
-
-            if attempt == 3:
-                raise KeyError
-
-
-    def get_robot_pose_matrix(self, x, y, z, rx, ry, rz):
-        R = Rotation.from_euler("ZYZ", [rx, ry, rz], degrees=True).as_matrix()
-        T = np.eye(4)
-        T[:3, :3] = R
-        T[:3, 3] = [x, y, z]
-        return T
-    
-
-    def get_target_pos(self, target):
-        self.get_position_request.target = target
-        self.get_logger().info("call depth position service with object_detection node")
-        get_position_future = self.get_position_client.call_async(
-            self.get_position_request
-        )
-        rclpy.spin_until_future_complete(self.node, get_position_future)
-
-        if get_position_future.result():
-            result = get_position_future.result().depth_position.tolist()
-            self.get_logger().info(f"Received depth position: {result}")
-            if sum(result) == 0:
-                self.get_logger().warn("No target position")
-                return None
-
-            gripper2cam_path = os.path.join(
-                package_path, "resource", "T_gripper2camera.npy"
-            )
-            robot_posx = self.DR.get_current_posx()[0]
-            td_coord = self.transform_to_base(result, gripper2cam_path, robot_posx)
-
-            if td_coord[2] and sum(td_coord) == 0:
-                raise KeyError
-
-            if td_coord[2] and sum(td_coord) != 0:
-                td_coord[2] += DEPTH_OFFSET  # DEPTH_OFFSET
-                td_coord[2] = max(td_coord[2], MIN_DEPTH)  # MIN_DEPTH: float = 2.0
-
-            target_pos = list(td_coord[:3]) + robot_posx[3:]
-            return target_pos
-        return None
-
-
-    def transform_to_base(self, camera_coords, gripper2cam_path, robot_pos):
-        """
-        Converts 3D coordinates from the camera coordinate system
-        to the robot's base coordinate system.
-        """
-        gripper2cam = np.load(gripper2cam_path)
-        coord = np.append(np.array(camera_coords), 1)  # Homogeneous coordinate
-
-        x, y, z, rx, ry, rz = robot_pos
-        base2gripper = self.get_robot_pose_matrix(x, y, z, rx, ry, rz)
-
-        # 좌표 변환 (그리퍼 → 베이스)
-        base2cam = base2gripper @ gripper2cam
-        td_coord = np.dot(base2cam, coord)
-
-        return td_coord[:3]
+        return self.success
+         
